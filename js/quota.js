efa6dd703b007cd7acbff07a972e8230af3f8d10
Sherman Ying <idiotioc@gmail.com>
2015-12-06 00:11:42 -0500

    Code cleanup
    JS:
    * Converted Magic Numbers into Constants.
    * Added New Game and Restart
    * Removed unnecessary variables
    * Increased functionality for accessor methods of GAME; if no element
    specified, then returns a copy of the entire variable
    * All game variables are now private and must be accessed with
    accessors.
    * The only mutators in the GAME class are newGame, restartGame, move.
    
    CSS:
    * Added transitions
    * Adjusted status and player constants
    
    HTML:
    * Added an extra footer row for game control buttons.

--- js/quota.js
+++ js/quota.js
@@ -1,28 +1,65 @@
 QUOTA = (function(base, $) {
-  var _game;
-  initialize = base.initialize = function() {
-    if (!_game)
-      _game = new base.GAME();
-    else
-      _game.newGame();
+  Object.defineProperty(base, 'PLAYER', {
+    value: 'player',
+    writable: false,
+    enumerable: true,
+    configurable: true
+  });
+  Object.defineProperty(base, 'STATUS', {
+    value: 'status',
+    writable: false,
+    enumerable: true,
+    configurable: true
+  });
+  Object.defineProperty(base, 'ACTIVE', {
+    value: -1,
+    writable: false,
+    enumerable: true,
+    configurable: true
+  });
+  Object.defineProperty(base, 'BLOCKED', {
+    value: 0,
+    writable: false,
+    enumerable: true,
+    configurable: true
+  });
+  Object.defineProperty(base, 'INACTIVE', {
+    value: 1,
+    writable: false,
+    enumerable: true,
+    configurable: true
+  });
+  Object.defineProperty(base, 'SIZE', {
+    value: 5,
+    writable: false,
+    enumerable: true,
+    configurable: true
+  });
+  const PLAYER = base.PLAYER;
+  const STATUS = base.STATUS;
+  const ACTIVE = base.ACTIVE;
+  const BLOCKED = base.BLOCKED;
+  const INACTIVE = base.INACTIVE;
+  const SIZE = base.SIZE;
 
-    for (var i = 0; i < _game.SIZE; i++) {
-      var cRow = selectRowSum(i);
-      var cCol = selectColSum(i);
-      cRow.click(
+  var _game;
+
+  initialize = base.initialize = function() {
+    for (var i = 0; i < SIZE; i++) {
+      selectRowSum(i).click(
         (function(row) {
           return function() {
             _game.printRow(row);
           };
         })(i));
-      cCol.click(
+      selectColSum(i).click(
         (function(col) {
           return function() {
             _game.printCol(col);
           };
         })(i));
 
-      for (var j = 0; j < _game.SIZE; j++) {
+      for (var j = 0; j < SIZE; j++) {
         for (var v = 1; v < 4; v++) {
           var cValue = selectBoardButton(i, j, v);
           cValue.click(
@@ -30,30 +67,18 @@ QUOTA = (function(base, $) {
               return function() {
                 var check = _game.move(row, col, val);
                 if (check) {
-                  var cValue = selectBoardButton(row, col, val);
-                  var cRow = selectRowSum(row);
-                  var cCol = selectColSum(col);
                   var cPlayer = _game.currentTurn() % 2 + 1;
-                  cValue.attr('player', cPlayer);
+                  selectBoardButton(row, col, val).attr(PLAYER, cPlayer);
 
-                  var rd = _game.rowDiff(row);
-                  var cd = _game.colDiff(col);
-                  var rs = _game.rowSpace(row);
-                  var cs = _game.colSpace(col);
-
-                  if (rd === 0) {
-                    cRow.attr('player', cPlayer);
-                  } else if (rd < 0 || rs === 0) {
-                    cRow.attr('player', 0);
+                  if (_game.rowDiff(row) === 0) {
+                    selectRowSum(row).attr(PLAYER, cPlayer);
                   } else {
-                    cRow.attr('player', _game.EMPTY);
+                    selectRowSum(row).attr(PLAYER, BLOCKED);
                   }
-                  if (cd === 0) {
-                    cCol.attr('player', cPlayer);
-                  } else if (cd < 0 || cs === 0) {
-                    cCol.attr('player', 0);
+                  if (_game.colDiff(col) === 0) {
+                    selectColSum(col).attr(PLAYER, cPlayer);
                   } else {
-                    cCol.attr('player', _game.EMPTY);
+                    selectColSum(col).attr(PLAYER, BLOCKED);
                   }
                   updateBoard();
                 }
@@ -62,8 +87,31 @@ QUOTA = (function(base, $) {
         }
       }
     }
+
+    $('input.new-game').click((function() {
+      _game.newGame();
+      resetColours();
+      updateBoard();
+    }));
+    $('input.restart-game').click((function() {
+      _game.restartGame();
+      resetColours();
+      updateBoard();
+    }));
+
+    _game = new base.GAME(SIZE);
+    resetColours();
     updateBoard();
   };
+  resetColours = base.resetColours = function() {
+    for (var i = 0; i < SIZE; i++) {
+      selectRowSum(i).attr('player', BLOCKED);
+      selectColSum(i).attr('player', BLOCKED);
+      for (var j = 0; j < SIZE; j++)
+        for (var v = 1; v < 4; v++)
+          selectBoardButton(i, j, v).attr(PLAYER, BLOCKED);
+    }
+  }
   selectBoardButton = base.selectBoardButton = function(i, j, v) {
     return $('input.value.r-' + (i + 1) + '.c-' + (j + 1) + '.v-' + (v));
   };
@@ -82,39 +130,57 @@ QUOTA = (function(base, $) {
   }
 
   updateBoard = base.updateBoard = function() {
-    var turnDisplay = selectTurnDisplay();
-    var p1Score = selectScoreDisplay(0);
-    var p2Score = selectScoreDisplay(1);
-    turnDisplay.html((_game.currentTurn()+1));
-    p1Score.html(_game.score(0));
-    p2Score.html(_game.score(1));
-    for (var i = 0; i < 5; i++) {
+    selectTurnDisplay().html((_game.currentTurn() + 1));
+    selectScoreDisplay(0).html(_game.score(0));
+    selectScoreDisplay(1).html(_game.score(1));
+
+    for (var i = 0; i < SIZE; i++) {
       var cRow = selectRowSum(i);
-      var cCol = selectColSum(j);
+      var cCol = selectColSum(i);
       cRow.val(_game.rowSum(i));
       cCol.val(_game.colSum(i));
-      for (var j = 0; j < 5; j++) {
+
+      var rd = _game.rowDiff(i);
+      var cd = _game.colDiff(i);
+      var rs = _game.rowSpace(i);
+      var cs = _game.colSpace(i);
+
+      if (rd === 0) {
+        cRow.attr(STATUS, INACTIVE);
+      } else if (rd < 0 || rs === 0) {
+        cRow.attr(STATUS, BLOCKED);
+      } else {
+        cRow.attr(STATUS, ACTIVE);
+      }
+      if (cd === 0) {
+        cCol.attr(STATUS, INACTIVE);
+      } else if (cd < 0 || cs === 0) {
+        cCol.attr(STATUS, BLOCKED);
+      } else {
+        cCol.attr(STATUS, ACTIVE);
+      }
+
+      for (var j = 0; j < _game.SIZE; j++) {
         var results = _game.checkMove(i, j);
         var range = results * 3;
-        for (var v = 1; v <= 3; v++) {
+        for (var v = 1; v < 4; v++) {
           var cValue = selectBoardButton(i, j, v);
           var entryValue = _game.boardState(i, j);
-          if (entryValue === _game.EMPTY) {
-            cValue.attr('status', range);
+          if (entryValue === ACTIVE) {
+            cValue.attr(STATUS, ACTIVE);
           } else if (entryValue !== v) {
-            cValue.attr('status', '0');
+            cValue.attr(STATUS, BLOCKED);
           } else {
-            cValue.attr('status', _game.EMPTY);
+            cValue.attr(STATUS, INACTIVE);
           }
         }
       }
     }
   }
-  GAME = base.GAME = function(base) {
-    if (typeof base === 'undefined')
-      base = {};
+  GAME = base.GAME = function(size) {
+    base = {};
 
-    Object.defineProperty(base, 'EMPTY', {
+    Object.defineProperty(base, 'ACTIVE', {
       value: -1,
       writable: false,
       enumerable: true,
@@ -126,14 +192,21 @@ QUOTA = (function(base, $) {
       enumerable: true,
       configurable: true
     });
-    Object.defineProperty(base, 'SIZE', {
-      value: 5,
+    Object.defineProperty(base, 'INACTIVE', {
+      value: 1,
       writable: false,
       enumerable: true,
       configurable: true
     });
-    const EMPTY = base.EMPTY;
+    Object.defineProperty(base, 'SIZE', {
+      value: size,
+      writable: false,
+      enumerable: true,
+      configurable: true
+    });
+    const ACTIVE = base.ACTIVE;
     const BLOCKED = base.BLOCKED;
+    const INACTIVE = base.INACTIVE;
     const SIZE = base.SIZE;
 
     createArray = base.createArray = function(length) {
@@ -145,16 +218,18 @@ QUOTA = (function(base, $) {
       }
       return arr;
     };
-    randomSum = base.randomSum = function() {
-      return Math.floor(Math.random() * 8 + 4);
+    randomSum = base.randomSum = function(range, offset) {
+      if (typeof range === 'undefined')
+        range = 1;
+      if (typeof offset === 'undefined')
+        offset = 0;
+      return Math.floor(Math.random() * range + offset);
     };
     var _board, _rowSum, _colSum, _rowTotal, _colTotal, _rowEntry, _colEntry, _turn, _scores;
 
-    newGame = base.newGame = function() {
+    restartGame = base.restartGame = function() {
       _board = createArray(SIZE, SIZE);
 
-      _rowSum = createArray(SIZE);
-      _colSum = createArray(SIZE);
       _rowTotal = createArray(SIZE);
       _colTotal = createArray(SIZE);
       _rowEntry = createArray(SIZE);
@@ -166,14 +241,22 @@ QUOTA = (function(base, $) {
       _scores[1] = 0;
 
       for (var i = 0; i < SIZE; i++) {
-        _rowSum[i] = randomSum();
-        _colSum[i] = randomSum();
         _colTotal[i] = _rowTotal[i] = _rowEntry[i] = _colEntry[i] = 0;
         for (var j = 0; j < SIZE; j++) {
-          _board[i][j] = EMPTY;
+          _board[i][j] = ACTIVE;
         }
       }
       validateBoard();
+    }
+    newGame = base.newGame = function() {
+      _rowSum = createArray(SIZE);
+      _colSum = createArray(SIZE);
+
+      for (var i = 0; i < SIZE; i++) {
+        _rowSum[i] = randomSum(8, 4);
+        _colSum[i] = randomSum(8, 4);
+      }
+      restartGame();
     };
 
     validateBoard = base.validateBoard = function() {
@@ -181,7 +264,7 @@ QUOTA = (function(base, $) {
         for (var j = 0; j < SIZE; j++) {
           var results = checkMove(i, j);
           var range = results * 3;
-          if (_board[i][j] === EMPTY && range === 0) {
+          if (_board[i][j] === ACTIVE && range === 0) {
             _board[i][j] = 0;
             _rowEntry[i]++;
             _colEntry[j]++;
@@ -199,62 +282,62 @@ QUOTA = (function(base, $) {
         _colTotal[col] += val;
         _rowEntry[row]++;
         _colEntry[col]++;
-        _scores[base.turn % 2] += (rowDiff(row) === 0) ? _rowSum[row] : 0;
-        _scores[base.turn % 2] += (colDiff(col) === 0) ? _colSum[col] : 0;
+        _scores[_turn % 2] += (rowDiff(row) === 0) ? _rowSum[row] : 0;
+        _scores[_turn % 2] += (colDiff(col) === 0) ? _colSum[col] : 0;
         _turn++;
         validateBoard();
       }
       return check;
     };
     checkMove = base.checkMove = function(row, col) {
-      var isEmpty = (_board[row][col] === EMPTY);
+      var isEmpty = (_board[row][col] === ACTIVE);
       var isRowIncomplete = (rowSpace(row) > 0) && (rowDiff(row) > 0);
       var isColIncomplete = (colSpace(col) > 0) && (colDiff(col) > 0);
       return (isEmpty && isRowIncomplete && isColIncomplete);
     };
 
     // Accessor Methods
-    rowSpace = base.rowSpace = function(i) {
-      if (typeof i !== 'undefined')
-        return SIZE - _rowEntry[i];
+    rowSpace = base.rowSpace = function(row) {
+      if (typeof row !== 'undefined')
+        return SIZE - _rowEntry[row];
       var space = createArray(SIZE);
-      for (i = 0; i < SIZE; i++) {
+      for (var i = 0; i < SIZE; i++) {
         space[i] = SIZE - _rowEntry[i];
       }
       return space;
     };
-    colSpace = base.colSpace = function(j) {
-      if (typeof j !== 'undefined')
-        return SIZE - _colEntry[j];
+    colSpace = base.colSpace = function(col) {
+      if (typeof col !== 'undefined')
+        return SIZE - _colEntry[col];
       var space = createArray(SIZE);
-      for (j = 0; j < SIZE; j++)
+      for (var j = 0; j < SIZE; j++)
         space[j] = SIZE - _colEntry[j];
       return space;
     };
 
-    rowDiff = base.rowDiff = function(i) {
-      if (typeof i !== 'undefined')
-        return _rowSum[i] - _rowTotal[i];
+    rowDiff = base.rowDiff = function(row) {
+      if (typeof row !== 'undefined')
+        return _rowSum[row] - _rowTotal[row];
       var diff = createArray(SIZE);
-      for (i = 0; i < SIZE; i++)
+      for (var i = 0; i < SIZE; i++)
         diff[i] = _rowSum[i] - _rowTotal[i];
       return diff;
     };
-    colDiff = base.colDiff = function(j) {
-      if (typeof j !== 'undefined')
-        return _colSum[j] - _colTotal[j];
+    colDiff = base.colDiff = function(col) {
+      if (typeof col !== 'undefined')
+        return _colSum[col] - _colTotal[col];
       var diff = createArray(SIZE);
-      for (j = 0; j < SIZE; j++)
+      for (var j = 0; j < SIZE; j++)
         diff[j] = _colSum[j] - _colTotal[j];
       return diff;
     };
 
-    boardState = base.boardState = function(i, j) {
-      if (typeof i !== 'undefined' && typeof j !== 'undefined')
-        return _board[i][j];
+    boardState = base.boardState = function(row, col) {
+      if (typeof row !== 'undefined' && typeof col !== 'undefined')
+        return _board[row][col];
       var state = createArray(SIZE, SIZE);
-      for (i = 0; i < SIZE; i++) {
-        for (j = 0; j < SIZE; j++) {
+      for (var i = 0; i < SIZE; i++) {
+        for (var j = 0; j < SIZE; j++) {
           state[i][j] = _board[i][j];
         }
       }
@@ -264,37 +347,76 @@ QUOTA = (function(base, $) {
       return _turn;
     };
 
-    rowSum = base.rowSum = function(i) {
-      return _rowSum[i];
-    };
-    colSum = base.colSum = function(j) {
-      return _colSum[j];
-    };
-    rowTotal = base.rowTotal = function(i) {
-      return _rowTotal[i];
-    };
-    colTotal = base.colTotal = function(j) {
-      return _colTotal[j];
-    };
-
-    score = base.score = function(p) {
-      return _scores[p];
-    };
-
-    printRow = base.printRow = function(i) {
-      var out = 'Row ' + i + ':';
-      for (var j = 0; j < SIZE; j++)
-        out += ' ' + _board[i][j];
-      console.log(out);
-    };
-    printCol = base.printCol = function(j) {
-      var out = 'Col ' + j + ':';
+    rowSum = base.rowSum = function(row) {
+      if (typeof row !== 'undefined')
+        return _rowSum[row];
+      var sum = createArray(5);
       for (var i = 0; i < SIZE; i++)
-        out += ' ' + _board[i][j];
+        sum[i] = _rowSum[i];
+      return sum;
+    };
+    colSum = base.colSum = function(col) {
+      if (typeof col !== 'undefined')
+        return _colSum[col];
+      var sum = createArray(5);
+      for (var j = 0; j < SIZE; j++)
+        sum[j] = _colSum[j];
+      return sum
+    };
+    rowTotal = base.rowTotal = function(row) {
+      if (typeof row !== 'undefined')
+        return _rowTotal[row];
+      var total = createArray(5);
+      for (var i = 0; i < SIZE; i++)
+        total[i] = _rowTotal[i];
+      return total;
+    };
+    colTotal = base.colTotal = function(col) {
+      if (typeof col !== 'undefined')
+        return _colTotal[col];
+      var total = createArray(5);
+      for (var j = 0; j < SIZE; j++)
+        total[j] = _colTotal[j];
+      return total;
+    };
+    rowEntry = base.rowEntry = function(row) {
+      if (typeof row !== 'undefined')
+        return _rowEntry[row];
+      var entry = createArray(5);
+      for (var i = 0; i < SIZE; i++)
+        entry[i] = _rowEntry[i];
+      return entry;
+    };
+    colEntry = base.colEntry = function(col) {
+      if (typeof col !== 'undefined')
+        return _colEntry[col];
+      var entry = createArray(5);
+      for (var j = 0; j < SIZE; j++)
+        entry[j] = _colEntry[j];
+      return entry;
+    };
+
+    score = base.score = function(playerNum) {
+      if (typeof playerNum !== 'undefined')
+        return _scores[playerNum];
+      var scores = [_scores[0], _scores[1]];
+      return scores;
+    };
+
+    printRow = base.printRow = function(row) {
+      var out = 'Row ' + row + ':';
+      for (var j = 0; j < SIZE; j++)
+        out += ' ' + _board[row][j];
+      console.log(out);
+    };
+    printCol = base.printCol = function(col) {
+      var out = 'Col ' + col + ':';
+      for (var i = 0; i < SIZE; i++)
+        out += ' ' + _board[i][col];
       console.log(out);
     };
 
-    base.newGame();
+    newGame();
     return base;
   };
   $(document).ready(function() {
